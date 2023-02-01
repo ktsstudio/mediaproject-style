13193d4a462a0260ab4288b57e66ddf657fc375b
n.merkulova <n.merkulova@ktsstudio.ru>
2023-02-01 18:29:50 +0400

    [*] issue #5

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 const getNumberProperty = (propertyName: string): number => {
   const computedStyle = getComputedStyle(
-    document.documentElement
+    document.getElementById('root') ?? document.documentElement
   ).getPropertyValue(propertyName);
 
   return computedStyle ? Number.parseInt(computedStyle) : 0;
